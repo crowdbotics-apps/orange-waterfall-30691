29a86f96b14b80826b828a2c54d80e03b865c794
Crowdbotics <team@crowdbotics.com>
2021-09-20 00:29:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "orange_waterfall_30691Identifier",
+  url: "https://orange_waterfall_30691Identifier.botics.co",
+  api: "https://orange_waterfall_30691Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
